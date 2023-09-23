873a644698532496bb2b25b976dcbd4254d2edd9
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-09-22 23:38:26 -0700

    Add a number of different LSPs.
    This commit adds the following LSPs:
      * bashls
      * cmake
      * dockerls
      * jedi_language_server
      * lua_ls
      * marksman
      * texlab

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,10 +4,38 @@ local capabilities = base.capabilities
 
 local lspconfig = require("lspconfig")
 
+lspconfig.bashls.setup {
+  capabilities = capabilities,
+}
+
 lspconfig.clangd.setup {
   on_attach = function(client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
     on_attach(client, bufnr)
   end,
+  capabilities = capabilities,
+}
+
+lspconfig.cmake.setup {
+  capabilities = capabilities,
+}
+
+lspconfig.dockerls.setup {
+  capabilities = capabilities,
+}
+
+lspconfig.jedi_language_server.setup {
+  capabilities = capabilities,
+}
+
+lspconfig.lua_ls.setup {
+  capabilities = capabilities,
+}
+
+lspconfig.marksman.setup {
+  capabilities = capabilities,
+}
+
+lspconfig.texlab.setup {
   capabilities = capabilities,
 }

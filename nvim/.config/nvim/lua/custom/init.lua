761c19821722cba44ce3143a3d7a2bd749477925
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2023-11-04 15:42:19 -0700

    Automatically set textwidth=80 when editing markdown files.

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -11,6 +11,14 @@ keymap("n", "<C-b>", ":SymbolsOutline<CR>", default_opts)
 -- Set the nterm shell to fish.
 vim.o.shell = "fish"
 
+-- Set textwidth=80 when editing markdown files.
+vim.cmd([[ 
+  augroup auFileTypes
+    autocmd!
+    autocmd FileType markdown setlocal textwidth=80
+  augroup end
+]])
+
 -- See https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428
 local notify = vim.notify
 vim.notify = function(msg, ...)

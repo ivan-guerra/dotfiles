ee347031f0ad9cec3539b7660eef8679e6564895
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-06-25 22:21:00 -0400

    Rename the pacclean alias to pacc.

---
+++ fish/.config/fish/functions/pacc.fish
@@ -0,0 +1,3 @@
+function pacc -d "Clean cache - delete all the package files in the cache"
+    sudo paccache -r
+end
--- fish/.config/fish/functions/pacclean.fish
+++
@@ -1,3 +0,0 @@
-function pacclean -d "Clean cache - delete all the package files in the cache"
-    sudo paccache -r
-end

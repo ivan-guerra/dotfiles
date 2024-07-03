72cabb73a892920f1bfef5ae64c8ae7436bdaa93
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-07-03 16:32:44 -0400

    Add the prettier formatter for HTML and CSS formatting.

--- nvim/.config/nvim/lua/custom/configs/null-ls.lua
+++ nvim/.config/nvim/lua/custom/configs/null-ls.lua
@@ -8,6 +8,7 @@ local opts = {
     null_ls.builtins.formatting.clang_format,
     null_ls.builtins.formatting.cmake_format,
     null_ls.builtins.formatting.latexindent,
+    null_ls.builtins.formatting.prettier,
     null_ls.builtins.diagnostics.cmake_lint,
     null_ls.builtins.diagnostics.cpplint,
     null_ls.builtins.diagnostics.flake8,

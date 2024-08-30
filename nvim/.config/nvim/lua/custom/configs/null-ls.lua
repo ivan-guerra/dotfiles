d2524fda0d4c9085f1ff47433026a0b95cceaa1b
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-08-29 23:45:14 -0400

    Use pylint and pydocstyle for Python linting.

--- nvim/.config/nvim/lua/custom/configs/null-ls.lua
+++ nvim/.config/nvim/lua/custom/configs/null-ls.lua
@@ -11,7 +11,8 @@ local opts = {
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.diagnostics.cmake_lint,
     null_ls.builtins.diagnostics.cpplint,
-    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.pylint,
+    null_ls.builtins.diagnostics.pydocstyle,
     null_ls.builtins.diagnostics.shellcheck,
     null_ls.builtins.diagnostics.vale
   },

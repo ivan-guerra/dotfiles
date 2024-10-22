ec18eded8ca2ca15a0ad715c1973003684f646e0
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-10-21 21:26:57 -0400

    Configure nvim for rust development.

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -2,4 +2,5 @@
 local M = {}
 M.ui = { theme = 'jellybeans' }
 M.plugins = "custom.plugins"
+M.mappings = require("custom.mappings")
 return M
--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -30,7 +30,7 @@ local plugins = {
     "jose-elias-alvarez/null-ls.nvim",
     event = "VeryLazy",
     opts = function()
-      return require "custom.configs.null-ls"
+      return require("custom.configs.null-ls")
     end,
   },
   {
@@ -38,16 +38,52 @@ local plugins = {
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
-      require('lspconfig').lua_ls.setup({
+      require("lspconfig").lua_ls.setup({
         settings = {
           Lua = {
             diagnostics = {
-              globals = {'vim'}
+              globals = {"vim"}
             }
           }
         }
       })
     end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft= "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    ft = { "rust" },
+    dependencies = "neovim/nvim-lspconfig",
+    config = function()
+      require "custom.configs.rustaceanvim"
+    end
+  },
+  {
+    "mfussenegger/nvim-dap"
+  },
+  {
+    "saecki/crates.nvim",
+    ft = {"rust", "toml"},
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require("plugins.configs.cmp")
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end
   },
   {
     "williamboman/mason.nvim",
@@ -67,6 +103,7 @@ local plugins = {
         "latexindent",
         "lua-language-server",
         "marksman",
+        "rust-analyzer",
         "shellcheck",
         "texlab",
         "vale"

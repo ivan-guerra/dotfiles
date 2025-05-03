bb86296cbef177cb89115e2e1a33c9d570a0aff1
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2025-05-03 17:47:04 -0400

    Install and enable the CMake linter correctly.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,147 +1,148 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre",
+		opts = require("configs.conform"),
+	},
 
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    lazy = false,
-    branch = "main",
-    dependencies = {
-      { "github/copilot.vim" },
-      { "nvim-lua/plenary.nvim" },
-    },
-    opts = require "configs.copilotchat",
-  },
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		lazy = false,
+		branch = "main",
+		dependencies = {
+			{ "github/copilot.vim" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+		opts = require("configs.copilotchat"),
+	},
 
-  {
-    "radenling/vim-dispatch-neovim",
-    dependencies = "tpope/vim-dispatch",
-    lazy = false,
-  },
+	{
+		"radenling/vim-dispatch-neovim",
+		dependencies = "tpope/vim-dispatch",
+		lazy = false,
+	},
 
-  {
-    "rust-lang/rust.vim",
-    ft = "rust",
-    init = function()
-      vim.g.rustfmt_autosave = 1
-    end,
-  },
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
 
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^5",
-    lazy = false,
-    ft = { "rust" },
-    dependencies = "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.rustaceanvim"
-    end,
-  },
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^5",
+		lazy = false,
+		ft = { "rust" },
+		dependencies = "neovim/nvim-lspconfig",
+		config = function()
+			require("configs.rustaceanvim")
+		end,
+	},
 
-  {
-    "saecki/crates.nvim",
-    ft = { "rust", "toml" },
-    config = function(_, opts)
-      local crates = require "crates"
-      crates.setup(opts)
-      crates.show()
-    end,
-  },
+	{
+		"saecki/crates.nvim",
+		ft = { "rust", "toml" },
+		config = function(_, opts)
+			local crates = require("crates")
+			crates.setup(opts)
+			crates.show()
+		end,
+	},
 
-  {
-    "mfussenegger/nvim-lint",
-    event = {
-      "BufReadPre",
-      "BufNewFile",
-    },
-    config = function()
-      require "configs.nvim-lint"
-    end,
-  },
+	{
+		"mfussenegger/nvim-lint",
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
+		config = function()
+			require("configs.nvim-lint")
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        -- language servers
-        "bash-language-server",
-        "clangd",
-        "cmake-language-server",
-        "dockerfile-language-server",
-        "json-lsp",
-        "lua-language-server",
-        "marksman",
-        "pyright",
-        "superhtml",
-        "texlab",
-        "vale-ls",
-        -- linters
-        "cmakelang",
-        "cpplint",
-        "flake8",
-        "hadolint",
-        "htmlhint",
-        "jsonlint",
-        "shellcheck",
-        "stylelint",
-        "vale",
-        -- formatters
-        "autopep8",
-        "beautysh",
-        "clang-format",
-        "prettier",
-        "prettierd",
-      },
-    },
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				-- language servers
+				"bash-language-server",
+				"clangd",
+				"cmake-language-server",
+				"dockerfile-language-server",
+				"json-lsp",
+				"lua-language-server",
+				"marksman",
+				"pyright",
+				"superhtml",
+				"texlab",
+				"vale-ls",
+				-- linters
+				"cmakelang",
+				"cmakelint",
+				"cpplint",
+				"flake8",
+				"hadolint",
+				"htmlhint",
+				"jsonlint",
+				"shellcheck",
+				"stylelint",
+				"vale",
+				-- formatters
+				"autopep8",
+				"beautysh",
+				"clang-format",
+				"prettier",
+				"prettierd",
+			},
+		},
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "vimdoc",
-        "html",
-        "css",
-        "rust",
-        "cpp",
-        "python",
-        "bash",
-        "markdown",
-        "dockerfile",
-        "cmake",
-        "devicetree",
-        "doxygen",
-        "gitcommit",
-        "gitattributes",
-        "gitignore",
-        "git_rebase",
-        "git_config",
-        "json",
-        "kconfig",
-        "just",
-        "muttrc",
-        "superhtml",
-        "tmux",
-        "xml",
-        "yaml",
-        "toml",
-        "rst",
-        "make",
-        "fish",
-        "diff",
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"vimdoc",
+				"html",
+				"css",
+				"rust",
+				"cpp",
+				"python",
+				"bash",
+				"markdown",
+				"dockerfile",
+				"cmake",
+				"devicetree",
+				"doxygen",
+				"gitcommit",
+				"gitattributes",
+				"gitignore",
+				"git_rebase",
+				"git_config",
+				"json",
+				"kconfig",
+				"just",
+				"muttrc",
+				"superhtml",
+				"tmux",
+				"xml",
+				"yaml",
+				"toml",
+				"rst",
+				"make",
+				"fish",
+				"diff",
+			},
+		},
+	},
 }

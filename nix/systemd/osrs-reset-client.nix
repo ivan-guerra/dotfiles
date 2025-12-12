0e7452677cf27e728cb2036a05abc2c8b465d626
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2025-12-11 22:48:24 -0500

    fix(systemd): update osrs-reset-client service options
    Changed the osrs-reset-client systemd service Type from "oneshot" to
    "simple" and set KillMode to "none" to prevent systemd from reaping
    child processes. This ensures the client process continues running as
    intended after the service starts.

--- nix/systemd/osrs-reset-client.nix
+++ nix/systemd/osrs-reset-client.nix
@@ -9,7 +9,11 @@
     };
 
     Service = {
-      Type = "oneshot";
+      Type = "simple";
+      # KillMode set to none means no processes spawned by the script will be killed
+      # when systemd finishes executing the script. Without this option systemd will
+      # reap the client itself.
+      KillMode = "none";
       Environment = "DISPLAY=:1";
       ExecStart = "${pkgs.bash}/bin/bash /home/ieg/dev/colorbot/rstart.sh";
     };

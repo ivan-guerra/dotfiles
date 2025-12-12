29333d7f8a81d20a22dde6b0db48f24ba30f814b
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2025-12-12 12:37:55 -0500

    feat(wallpapers): switch default wallpaper to jupiter

--- nix/apps/wallpapers.nix
+++ nix/apps/wallpapers.nix
@@ -5,9 +5,9 @@ let
     owner = "ivan-guerra";
     repo = "dotfiles";
     rev = "nixos";
-    sha256 = "sha256-SEOKRXv8NnyT+kXwnd7Y53Hsek27+gkTcvn7QFUfEno=";
+    sha256 = "sha256-TtSVXKj/lxdL+vQCS2HQsmjKzW3AMuREBJUWe/xWoLQ=";
   };
 in
 {
-  home.file.".background-image".source = "${dotfiles}/wallpapers/sparks.jpg";
+  home.file.".background-image".source = "${dotfiles}/wallpapers/jupiter.png";
 }

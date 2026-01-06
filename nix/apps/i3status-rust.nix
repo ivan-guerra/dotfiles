026e60a4ddd7f937f4c2c790a26b58513cca6358
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2026-01-06 16:14:44 -0500

    fix(i3status-rust): update hardware-specific config
    - Update temperature chip from generic to k10temp-pci-* for AMD
    - Change network device from wlp1s0 to wlp3s0
    - Remove battery block (not applicable to desktop system)
    
    These changes adapt the bar to the MinisForum U870M MiniPC.

--- nix/apps/i3status-rust.nix
+++ nix/apps/i3status-rust.nix
@@ -17,7 +17,7 @@
           {
             block = "temperature";
             interval = 1;
-            chip = "*-isa-*";
+            chip = "k10temp-pci-*";
             format = " $icon $max ";
           }
           
@@ -32,7 +32,7 @@
           
           {
             block = "net";
-            device = "wlp1s0";
+            device = "wlp3s0";
             interval = 5;
             format = " $icon {$ssid} ";
             format_alt = " $icon {$signal_strength $ssid $frequency $bitrate|Disconnected} ";
@@ -44,15 +44,6 @@
             format = " $icon {$volume|Muted} ";
             max_vol = 100;
             step_width = 5;
-          }
-          
-          {
-            block = "battery";
-            interval = 10;
-            format = " $icon $percentage {$time|} ";
-            device = "BAT0";
-            full_format = " $icon $percentage ";
-            missing_format = "";
           }
           
           {

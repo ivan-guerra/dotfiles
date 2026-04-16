8b6b71b19623b191f83f272fbe25c2e326c6cfdd
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2026-04-15 23:39:11 -0400

    refactor(plugins): remove rust.vim plugin
    Remove rust.vim as rustaceanvim already provides Rust language support
    including formatting capabilities, making rust.vim redundant.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -52,14 +52,6 @@ return {
   },
 
   {
-    "rust-lang/rust.vim",
-    ft = "rust",
-    init = function()
-      vim.g.rustfmt_autosave = 1
-    end,
-  },
-
-  {
     "mrcjkb/rustaceanvim",
     version = "^5",
     ft = "rust",
